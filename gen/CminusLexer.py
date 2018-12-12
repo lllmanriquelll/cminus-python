da3a310ac569cbd33cbbecd487d90be9f5fbc02d
manriqueluis <manrique.luis@gmail.com>
2018-12-12 10:54:08 -0200

    First Commint

---
+++ gen/CminusLexer.py
@@ -0,0 +1,142 @@
+# Generated from Cminus.g4 by ANTLR 4.7.1
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\37")
+        buf.write("\u00a9\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30")
+        buf.write("\4\31\t\31\4\32\t\32\4\33\t\33\4\34\t\34\4\35\t\35\4\36")
+        buf.write("\t\36\3\2\3\2\3\2\3\2\3\3\3\3\3\3\3\3\3\3\3\4\3\4\3\4")
+        buf.write("\7\4J\n\4\f\4\16\4M\13\4\5\4O\n\4\3\5\3\5\3\6\3\6\3\7")
+        buf.write("\3\7\3\b\3\b\3\t\3\t\3\n\3\n\3\13\3\13\3\f\3\f\3\r\3\r")
+        buf.write("\3\r\3\16\3\16\3\16\3\16\3\16\3\17\3\17\3\17\3\17\3\17")
+        buf.write("\3\17\3\20\3\20\3\20\3\20\3\20\3\20\3\20\3\21\6\21w\n")
+        buf.write("\21\r\21\16\21x\3\22\3\22\3\23\3\23\3\23\3\24\3\24\3\25")
+        buf.write("\3\25\3\26\3\26\3\26\3\27\3\27\3\27\3\30\3\30\3\30\3\31")
+        buf.write("\3\31\3\32\3\32\3\33\3\33\3\34\3\34\3\35\3\35\3\35\3\35")
+        buf.write("\7\35\u0099\n\35\f\35\16\35\u009c\13\35\3\35\3\35\3\35")
+        buf.write("\3\35\3\35\3\36\6\36\u00a4\n\36\r\36\16\36\u00a5\3\36")
+        buf.write("\3\36\3\u009a\2\37\3\3\5\4\7\5\t\6\13\7\r\b\17\t\21\n")
+        buf.write("\23\13\25\f\27\r\31\16\33\17\35\20\37\21!\22#\23%\24\'")
+        buf.write("\25)\26+\27-\30/\31\61\32\63\33\65\34\67\359\36;\37\3")
+        buf.write("\2\6\3\2\63;\3\2\62;\4\2C\\c|\5\2\13\f\17\17\"\"\2\u00ad")
+        buf.write("\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3\2\2\2\2\13")
+        buf.write("\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3")
+        buf.write("\2\2\2\2\25\3\2\2\2\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2")
+        buf.write("\2\2\2\35\3\2\2\2\2\37\3\2\2\2\2!\3\2\2\2\2#\3\2\2\2\2")
+        buf.write("%\3\2\2\2\2\'\3\2\2\2\2)\3\2\2\2\2+\3\2\2\2\2-\3\2\2\2")
+        buf.write("\2/\3\2\2\2\2\61\3\2\2\2\2\63\3\2\2\2\2\65\3\2\2\2\2\67")
+        buf.write("\3\2\2\2\29\3\2\2\2\2;\3\2\2\2\3=\3\2\2\2\5A\3\2\2\2\7")
+        buf.write("N\3\2\2\2\tP\3\2\2\2\13R\3\2\2\2\rT\3\2\2\2\17V\3\2\2")
+        buf.write("\2\21X\3\2\2\2\23Z\3\2\2\2\25\\\3\2\2\2\27^\3\2\2\2\31")
+        buf.write("`\3\2\2\2\33c\3\2\2\2\35h\3\2\2\2\37n\3\2\2\2!v\3\2\2")
+        buf.write("\2#z\3\2\2\2%|\3\2\2\2\'\177\3\2\2\2)\u0081\3\2\2\2+\u0083")
+        buf.write("\3\2\2\2-\u0086\3\2\2\2/\u0089\3\2\2\2\61\u008c\3\2\2")
+        buf.write("\2\63\u008e\3\2\2\2\65\u0090\3\2\2\2\67\u0092\3\2\2\2")
+        buf.write("9\u0094\3\2\2\2;\u00a3\3\2\2\2=>\7k\2\2>?\7p\2\2?@\7v")
+        buf.write("\2\2@\4\3\2\2\2AB\7x\2\2BC\7q\2\2CD\7k\2\2DE\7f\2\2E\6")
+        buf.write("\3\2\2\2FO\7\62\2\2GK\t\2\2\2HJ\t\3\2\2IH\3\2\2\2JM\3")
+        buf.write("\2\2\2KI\3\2\2\2KL\3\2\2\2LO\3\2\2\2MK\3\2\2\2NF\3\2\2")
+        buf.write("\2NG\3\2\2\2O\b\3\2\2\2PQ\7=\2\2Q\n\3\2\2\2RS\7]\2\2S")
+        buf.write("\f\3\2\2\2TU\7_\2\2U\16\3\2\2\2VW\7*\2\2W\20\3\2\2\2X")
+        buf.write("Y\7+\2\2Y\22\3\2\2\2Z[\7.\2\2[\24\3\2\2\2\\]\7}\2\2]\26")
+        buf.write("\3\2\2\2^_\7\177\2\2_\30\3\2\2\2`a\7k\2\2ab\7h\2\2b\32")
+        buf.write("\3\2\2\2cd\7g\2\2de\7n\2\2ef\7u\2\2fg\7g\2\2g\34\3\2\2")
+        buf.write("\2hi\7y\2\2ij\7j\2\2jk\7k\2\2kl\7n\2\2lm\7g\2\2m\36\3")
+        buf.write("\2\2\2no\7t\2\2op\7g\2\2pq\7v\2\2qr\7w\2\2rs\7t\2\2st")
+        buf.write("\7p\2\2t \3\2\2\2uw\t\4\2\2vu\3\2\2\2wx\3\2\2\2xv\3\2")
+        buf.write("\2\2xy\3\2\2\2y\"\3\2\2\2z{\7?\2\2{$\3\2\2\2|}\7>\2\2")
+        buf.write("}~\7?\2\2~&\3\2\2\2\177\u0080\7>\2\2\u0080(\3\2\2\2\u0081")
+        buf.write("\u0082\7@\2\2\u0082*\3\2\2\2\u0083\u0084\7@\2\2\u0084")
+        buf.write("\u0085\7?\2\2\u0085,\3\2\2\2\u0086\u0087\7?\2\2\u0087")
+        buf.write("\u0088\7?\2\2\u0088.\3\2\2\2\u0089\u008a\7#\2\2\u008a")
+        buf.write("\u008b\7?\2\2\u008b\60\3\2\2\2\u008c\u008d\7-\2\2\u008d")
+        buf.write("\62\3\2\2\2\u008e\u008f\7/\2\2\u008f\64\3\2\2\2\u0090")
+        buf.write("\u0091\7,\2\2\u0091\66\3\2\2\2\u0092\u0093\7\61\2\2\u0093")
+        buf.write("8\3\2\2\2\u0094\u0095\7\61\2\2\u0095\u0096\7,\2\2\u0096")
+        buf.write("\u009a\3\2\2\2\u0097\u0099\13\2\2\2\u0098\u0097\3\2\2")
+        buf.write("\2\u0099\u009c\3\2\2\2\u009a\u009b\3\2\2\2\u009a\u0098")
+        buf.write("\3\2\2\2\u009b\u009d\3\2\2\2\u009c\u009a\3\2\2\2\u009d")
+        buf.write("\u009e\7,\2\2\u009e\u009f\7\61\2\2\u009f\u00a0\3\2\2\2")
+        buf.write("\u00a0\u00a1\b\35\2\2\u00a1:\3\2\2\2\u00a2\u00a4\t\5\2")
+        buf.write("\2\u00a3\u00a2\3\2\2\2\u00a4\u00a5\3\2\2\2\u00a5\u00a3")
+        buf.write("\3\2\2\2\u00a5\u00a6\3\2\2\2\u00a6\u00a7\3\2\2\2\u00a7")
+        buf.write("\u00a8\b\36\2\2\u00a8<\3\2\2\2\b\2KNx\u009a\u00a5\3\b")
+        buf.write("\2\2")
+        return buf.getvalue()
+
+
+class CminusLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    INT = 1
+    VOID = 2
+    NUMBER = 3
+    SEMI = 4
+    LBRACK = 5
+    RBRACK = 6
+    LPAREN = 7
+    RPAREN = 8
+    COMMA = 9
+    LBRACE = 10
+    RBRACE = 11
+    IF = 12
+    ELSE = 13
+    WHILE = 14
+    RETURN = 15
+    ID = 16
+    EQUAL = 17
+    LE = 18
+    LT = 19
+    GT = 20
+    GE = 21
+    EQUAL_EQUAL = 22
+    NOT_EQUAL = 23
+    ADD = 24
+    SUB = 25
+    MUL = 26
+    DIV = 27
+    COMMENT = 28
+    WS = 29
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'int'", "'void'", "';'", "'['", "']'", "'('", "')'", "','", 
+            "'{'", "'}'", "'if'", "'else'", "'while'", "'return'", "'='", 
+            "'<='", "'<'", "'>'", "'>='", "'=='", "'!='", "'+'", "'-'", 
+            "'*'", "'/'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "INT", "VOID", "NUMBER", "SEMI", "LBRACK", "RBRACK", "LPAREN", 
+            "RPAREN", "COMMA", "LBRACE", "RBRACE", "IF", "ELSE", "WHILE", 
+            "RETURN", "ID", "EQUAL", "LE", "LT", "GT", "GE", "EQUAL_EQUAL", 
+            "NOT_EQUAL", "ADD", "SUB", "MUL", "DIV", "COMMENT", "WS" ]
+
+    ruleNames = [ "INT", "VOID", "NUMBER", "SEMI", "LBRACK", "RBRACK", "LPAREN", 
+                  "RPAREN", "COMMA", "LBRACE", "RBRACE", "IF", "ELSE", "WHILE", 
+                  "RETURN", "ID", "EQUAL", "LE", "LT", "GT", "GE", "EQUAL_EQUAL", 
+                  "NOT_EQUAL", "ADD", "SUB", "MUL", "DIV", "COMMENT", "WS" ]
+
+    grammarFileName = "Cminus.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.7.1")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
