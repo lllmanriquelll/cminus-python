c7926dd04a7464ea179c264175ae3478636494c8
manriqueluis <manrique.luis@gmail.com>
2018-12-12 22:31:54 -0200

    test files added

--- myCminus.py
+++ myCminus.py
@@ -1,1 +1,10 @@
-import os
+from antlr4 import *
+from gen.CminusLexer import CminusLexer
+from gen.CminusParser import CminusParser
+
+input_stream = FileStream('files/code_test.c-')
+lexer = CminusLexer(input_stream)
+stream = CommonTokenStream(lexer)
+parser = CminusParser(stream)
+tree = parser.program()
+
